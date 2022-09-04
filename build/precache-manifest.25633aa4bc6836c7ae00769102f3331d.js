37cdf68d597ac1c83208fd065609dd177571814c
Devansh Mehra <81522384+Devansh-Mehra@users.noreply.github.com>
2022-09-04 11:43:14 +0530

    Deployed build pages

---
+++ build/precache-manifest.25633aa4bc6836c7ae00769102f3331d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ad135f261ab3bab916299a46f9b2e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e465b2af3a64fd1dd30",
+    "url": "/static/css/2.6a23f8e0.chunk.css"
+  },
+  {
+    "revision": "b786b48a2143d104dac3",
+    "url": "/static/css/main.242f13b4.chunk.css"
+  },
+  {
+    "revision": "3e465b2af3a64fd1dd30",
+    "url": "/static/js/2.3787bce3.chunk.js"
+  },
+  {
+    "revision": "b786b48a2143d104dac3",
+    "url": "/static/js/main.b31d76cb.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "35ba4ddd79e99d9c7c5dcca00b4c3035",
+    "url": "/static/media/about-img.35ba4ddd.png"
+  },
+  {
+    "revision": "dd263ba99f8edb10808f3005584c72e9",
+    "url": "/static/media/image-1.jpg.dd263ba9.jpg"
+  },
+  {
+    "revision": "809ce4f460d4fa962f70a7576178c8ad",
+    "url": "/static/media/img-1.809ce4f4.jpg"
+  },
+  {
+    "revision": "90f50ea8b0ed948eb2f79a2c3264d7db",
+    "url": "/static/media/img-2.90f50ea8.jpg"
+  },
+  {
+    "revision": "93ab2971c7f06030eb81516326080b0b",
+    "url": "/static/media/img-3.93ab2971.jpg"
+  }
+]);
